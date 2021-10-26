b711a152045f082b4416cdcb15b5280c53f66675
Rolwane Borges <rolwane@hotmail.com>
2021-10-26 18:03:56 -0300

    Requiitos 11 e 12

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // minha OL
 const lista = document.querySelector('#lista-tarefas');
 
+function loadTasks() {
+
+  if (!localStorage.items) {
+    localStorage.items = '[]';
+  }
+
+  let items = JSON.parse(localStorage.items);
+  for (const item of items) {
+    const li = document.createElement('li');
+    li.innerText = item.content;
+    li.className = item.classes;
+    lista.appendChild(li);
+  }
+}
+
+loadTasks();
+
 function addTask() {
   const input = document.querySelector('#texto-tarefa');
   const li = document.createElement('li');
@@ -43,8 +60,22 @@ function clearCompleted() {
   }
 }
 
+function saveTasks() {
+  const lis = document.querySelectorAll('li');
+
+  let arrLis = [];
+  for (let i = 0; i < lis.length; i++) {
+    let obj = { content: '', classes: ''}
+    obj.content = lis[i].innerText;
+    obj.classes = lis[i].className;
+    arrLis.push(obj);
+  }
+  localStorage.items = JSON.stringify(arrLis);
+}
+
 document.querySelector('#criar-tarefa').addEventListener('click', addTask);
 lista.addEventListener('click', selectTask);
 lista.addEventListener('dblclick', taskDone);
 document.querySelector('#apaga-tudo').addEventListener('click', clearAll);
 document.querySelector('#remover-finalizados').addEventListener('click', clearCompleted);
+document.querySelector('#salvar-tarefas').addEventListener('click', saveTasks);
