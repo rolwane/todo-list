5707b19adeb8cadfbbe3b205c18d2e083a52ad60
Rolwane Borges <rolwane@hotmail.com>
2021-10-26 15:45:17 -0300

    Requisito 07 e 08

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function addTask() {
-  let input = document.querySelector('#texto-tarefa');
-  let li = document.createElement('li');
+  const input = document.querySelector('#texto-tarefa');
+  const li = document.createElement('li');
   li.innerText = input.value;
   document.querySelector('#lista-tarefas').appendChild(li);
   input.value = '';
 }
 
+function selectTask(e) {
+  if (e.target.id !== 'lista-tarefas') {
+    if (e.target.classList.contains('selected')) {
+      e.target.classList.remove('selected');
+    } else {
+      if (document.querySelector('.selected')) {
+        document.querySelector('.selected').classList.remove('selected');
+      }
+      e.target.classList.add('selected');
+    }
+  }
+}
+
 document.querySelector('#criar-tarefa').addEventListener('click', addTask);
+document.querySelector('#lista-tarefas').addEventListener('click', selectTask);
